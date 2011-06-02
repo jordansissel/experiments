0b7bc363b42016dedeabd17d79e8eb3f74249615
Jordan Sissel <jls@semicomplete.com>
2011-06-02 09:31:45 -0700

    - take args - write more data per 'write' call

--- ruby/sockets/flood.rb
+++ ruby/sockets/flood.rb
@@ -2,9 +2,10 @@
 
 require "socket"
 
-sock = TCPSocket.new("127.0.0.1", 19384)
+#sock = TCPSocket.new("127.0.0.1", 19384)
+sock = TCPSocket.new(ARGV[0], ARGV[1].to_i)
 while true
-  sock.write("<13>May 19 18:30:17 snack jls: foo bar 32\n")
+  sock.write("<13>May 19 18:30:17 snack jls: foo bar 32\n" * 20)
 end
 
 #sock = UDPSocket.new
