ea1cd6f65b3dbbb3e474da208fc8fa79b7656be6
Jordan Sissel <jls@semicomplete.com>
2011-06-01 17:51:55 -0700

    - run flooder in a separate thread.

--- java/netty/SimpleClient.java
+++ java/netty/SimpleClient.java
@@ -17,19 +17,37 @@ public class SimpleClient {
   public class SimpleClientHandler extends SimpleChannelHandler {
     @Override
     public void channelConnected(ChannelHandlerContext context, ChannelStateEvent event) {
-      System.out.println("Ready");
+      final ChannelStateEvent event_ = event;
+
+      /* Run the flooder in a separate thread */
+      Thread flooder = new Thread(new Runnable() {
+        public void run() {
+          flood(event_.getChannel());
+        }
+      });
+
+      flooder.start();
+    } /* def channelConnected */
+
+    public void exceptionCaught(ChannelHandlerContext context, ExceptionEvent event) {
+      event.getCause().printStackTrace();
+      event.getChannel().close();
+    }
+
+    public void flood(Channel channel) {
       ChannelBuffer input = ChannelBuffers.copiedBuffer(
-        "<13>May 19 18:30:17 snack jls: foo bar 32\n",
-        "US-ASCII"
+        "<13>May 19 18:30:17 snack jls: foo bar 32\n".getBytes()
       );
 
       int count = 0;
+      int bytes = 0;
       long sleeptime_ms;
-      Channel channel = event.getChannel();
+
+      System.out.println("Ready");
       while (true) {
         sleeptime_ms = 1;
         while (!channel.isWritable()) {
-          System.out.println("busy... " + sleeptime_ms + " sleep_ms");
+          System.out.println("busy... " + sleeptime_ms + " sleep_ms, " + bytes + " bytes written");
           try {
             Thread.sleep(sleeptime_ms);
           } catch (InterruptedException e) {
@@ -39,18 +57,14 @@ public class SimpleClient {
         }
 
         channel.write(input);
-        //input.readerIndex(0)
+        input.readerIndex(0);
+        count += 1;
+        bytes += input.readableBytes();
         if (count % 100000 == 0) {
           System.out.println(count);
-          count += 1;
         }
       } /* while true */
-    } /* def channelConnected */
-
-    public void exceptionCaught(ChannelHandlerContext context, ExceptionEvent event) {
-      event.getCause().printStackTrace();
-      event.getChannel().close();
-    }
+    } /* void flood() */
   } /* class SimpleClientHandler */
 
   private NioClientSocketChannelFactory factory;
