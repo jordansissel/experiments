61e0c7855f3c3ff76edba6d8aba8b14a7192ad06
Jordan Sissel <jls@semicomplete.com>
2011-06-25 22:20:25 -0700

    - disable some debugging prints

--- c/libev/syslog/loggly_connection.c
+++ c/libev/syslog/loggly_connection.c
@@ -2,14 +2,14 @@
 
 void loggly_input_connection_stop(struct ev_loop *loop,
                                   loggly_input_connection *connection) {
-  printf("Closing %d\n", connection->io.fd);
+  //printf("Closing %d\n", connection->io.fd);
   ev_io_stop(loop, &connection->io);
   close(connection->io.fd);
   loggly_input_connection_free(connection);
 } /* loggly_input_connection_stop */
 
 void loggly_input_connection_free(loggly_input_connection *connection) {
-  printf("Freeing %x\n", (void *) connection->buffer);
+  //printf("Freeing %x\n", (void *) connection->buffer);
   if (connection != NULL) {
     if (connection->buffer != NULL) {
       free(connection->buffer);
@@ -22,7 +22,7 @@ void loggly_input_connection_free(loggly_input_connection *connection) {
 
 void loggly_input_stream_cb(struct ev_loop *loop, ev_io *watcher, int revents) {
   loggly_input_connection *connection = (loggly_input_connection*)watcher;
-  printf("Watcher %d ready\n", watcher->fd);
+  //printf("Watcher %d ready\n", watcher->fd);
 
   int rc;
   ssize_t bytes;
@@ -30,7 +30,6 @@ void loggly_input_stream_cb(struct ev_loop *loop, ev_io *watcher, int revents) {
 
   while (!done) {
     bytes = read(watcher->fd, connection->buffer, connection->buffer_len);
-    printf("Received %d bytes: '%.*s'\n", bytes, bytes, connection->buffer);
     if (bytes == 0) {
       loggly_input_connection_stop(EV_A_ connection); /* EOF, close it. */
       done = 1;
@@ -45,11 +44,12 @@ void loggly_input_stream_cb(struct ev_loop *loop, ev_io *watcher, int revents) {
     } /* bytes checking */
 
     /* Get here, and we have data. */
+    printf("Received %d bytes: '%.*s'\n", bytes, bytes, connection->buffer);
   } /* while true */
 } /* loggly_input_stream_cb */
 
 void loggly_input_datagram_cb(struct ev_loop *loop, ev_io *watcher,
                               int revents) {
-  /* Nothing to do yet, udp support and whatnot. */
+  /* Nothing to do yet. UDP support and whatnot. */
 }
 
