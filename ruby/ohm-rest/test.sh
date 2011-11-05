d1c5cc5f598cc64bf0e2fdd45572b8be3c3f42c0
Jordan Sissel <jls@semicomplete.com>
2011-11-05 00:17:02 -0700

    - Add resolve_all query to the test

--- ruby/ohm-rest/test.sh
+++ ruby/ohm-rest/test.sh
@@ -19,5 +19,9 @@ run curl -XPUT http://localhost:4567/host/testing/link/deployment/production
 # Add the 'frontend' role to 'testing' host
 run curl -XPUT http://localhost:4567/host/testing/link/role/frontend
 
-# Now show the 'testing' host.
+# Now show the 'testing' host. This will show the host and the paths to any
+# links.
 run curl -XGET http://localhost:4567/host/testing
+
+# Now show the 'testing' host with all links fully resolved to their objects.
+run curl -XGET http://localhost:4567/host/testing?resolve_all
