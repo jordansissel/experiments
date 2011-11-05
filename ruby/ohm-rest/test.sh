9a9915363f5176b0f53a1354c495b0bbc8506f32
Jordan Sissel <jls@semicomplete.com>
2011-11-05 02:13:43 -0700

    - make test.sh output markdown-ish

--- ruby/ohm-rest/test.sh
+++ ruby/ohm-rest/test.sh
@@ -1,40 +1,43 @@
 run() {
-  echo "=> $@"
-  "$@"
-  exit=$?
-  echo
+  (
+    echo
+    echo "% $@"
+    "$@"
+    exit=$?
+    echo
+    echo
+  ) | sed -e 's/^/    /'
 }
 
-# Create a deployment named 'production'
-run curl -XPUT http://localhost:4567/deployment/production
+echo "Create a deployment named 'production'"
+run curl -s -XPUT http://localhost:4567/deployment/production
 
-# Create a role named 'frontend'
-run curl -XPUT http://localhost:4567/role/frontend
+echo "Create a role named 'frontend'"
+run curl -s -XPUT http://localhost:4567/role/frontend
 
-# Create a role named 'monitor'
-run curl -XPUT http://localhost:4567/role/monitor
+echo "Create a role named 'monitor'"
+run curl -s -XPUT http://localhost:4567/role/monitor
 
-# Create a host named "testing" that's "booting"
-run curl -XPUT http://localhost:4567/host/testing -d '{ "state": "booting" }'
+echo "Create a host named 'testing' that has state 'booting'"
+run curl -s -XPUT http://localhost:4567/host/testing -d '{ "state": "booting" }'
 
-# Put the 'testing' host in 'production' deployment
-run curl -XPUT http://localhost:4567/host/testing/link/deployment/production
+echo "Put the 'testing' host in 'production' deployment"
+run curl -s -XPUT http://localhost:4567/host/testing/link/deployment/production
 
-# Add the 'frontend' role to 'testing' host
-run curl -XPUT http://localhost:4567/host/testing/link/role/frontend
+echo "Add the 'frontend' role to 'testing' host"
+run curl -s -XPUT http://localhost:4567/host/testing/link/role/frontend
 
-# Add the 'monitor' role to 'testing' host
-run curl -XPUT http://localhost:4567/host/testing/link/role/monitor
+echo "Add the 'monitor' role to 'testing' host"
+run curl -s -XPUT http://localhost:4567/host/testing/link/role/monitor
 
-# Now show the 'testing' host. This will show the host and the paths to any
-# links.
-run curl -XGET http://localhost:4567/host/testing
+echo "Now show the 'testing' host. This will show the host and the paths to any links"
+run curl -s -XGET http://localhost:4567/host/testing
 
-# Now show the 'testing' host with all links fully resolved to their objects.
-run curl -XGET http://localhost:4567/host/testing?resolve_all
+echo "Now show the 'testing' host with all links fully resolved to their objects."
+run curl -s -XGET http://localhost:4567/host/testing?resolve_all
 
-# Show all links for this entry:
-run curl -XGET http://localhost:4567/host/testing/link
+echo "Show all links for this entry:"
+run curl -s -XGET http://localhost:4567/host/testing/link
 
-# Show all only role links for this entry:
-run curl -XGET http://localhost:4567/host/testing/link/role
+echo "Show all only role links for this entry:"
+run curl -s -XGET http://localhost:4567/host/testing/link/role
