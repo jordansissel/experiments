ccf3127a74444a5e423c64265524aacfb0038928
Jordan Sissel <jls@semicomplete.com>
2012-01-01 19:02:18 -0800

    - horray

--- github/stats.sh
+++ github/stats.sh
@@ -1,5 +1,11 @@
 
 user="jordansissel"
-curl http://github.com/api/v2/json/repos/show/$user \
-| jgrep -s repositories.name | sed -e "s,.*,git@github.com:$user/&.git," \
-| xargs -P2 -n1 git clone
+#curl http://github.com/api/v2/json/repos/show/$user \
+#| jgrep -s repositories.name | sed -e "s,.*,git@github.com:$user/&.git," \
+#| xargs -P2 -n1 git clone
+
+for repo in $(find ./ -name '*.git' | xargs -n1 dirname); do
+  cd $repo
+  git log --date=rfc  --shortstat --format=short --since=2011 --before=2012 \
+  | grep 'files changed' | sed -e "s,^,$1: ,"
+done
