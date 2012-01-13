7839e788343b6e981942ed119adf965cd1de54f7
Jordan Sissel <jls@semicomplete.com>
2012-01-12 17:29:12 -0800

    - playing with emberjs

--- js/ember/foo/js/app.js
+++ js/ember/foo/js/app.js
@@ -1,7 +1,39 @@
-var App = Em.Application.create();
+var Heumon = Ember.Application.create();
 
-App.MyView = Em.View.extend({
+Heumon.Event = Ember.Object.extend({
+  name: null, /* The name of the event */
+
+  init: function(data) {
+    console.log(data);
+    this._super(data);
+  }
+}); /* Heumon.Event */
+
+Heumon.EventInstance = Ember.Object.extend({
+  event_name: null, /* The name of the event (See Heumon.Event) */
+  timestamp: null,
+
+  init: function(data) {
+    this.timestamp = new Date();
+    this._super(data);
+  }
+}); /* Heumon.EventInstance */
+
+Heumon.EventCollectionController = Ember.ArrayController.create({
+  content: [], /* the array backing this controller */
+
+  loadEvents: function() {
+    this.pushObject(Heumon.Event.create({ name: "diaper" }));
+    this.pushObject(Heumon.Event.create({ name: "ibuprofen" }));
+  },
+}); /* Heumon.EventCollectionController */
+
+Heumon.EventCollectionView = Ember.View.extend({
+});
+
+Heumon.MyView = Ember.View.extend({
   mouseDown: function() {
-    window.alert("hello world!");
+    var e = Heumon.EventInstance.create({ name: "diaper" })
+    console.log(e);
   }
 });
