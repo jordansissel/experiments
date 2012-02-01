c502c9a50a3eeac75c858f80a211a0ee2b82dbf9
Jordan Sissel <jls@semicomplete.com>
2012-02-01 15:32:54 -0800

    - cabin channels done - JSON stuff seems happy.

--- go/cabin/cabin.go
+++ go/cabin/cabin.go
@@ -1,70 +1,40 @@
 package cabin
 
 import (
-  "fmt"
-  "reflect"
   "time"
 )
 
 /* Make a new struct, but for now it has no members.
  * Later I will add channels and such for subscription stuff
  */
-type Cabin struct {}
+type Cabin struct {
+  /* Channels receive Event structs */
+  channels []chan *Event
+}
 
 /* A cabin event. Simply a timestamp + an object */
 type Event struct {
-  timestamp *time.Time
-  object *interface{}
+  Timestamp *time.Time
+  Message string
+  Object interface{}
 }
 
 /* Create a new Cabin instance */
 func New() *Cabin {
-  return new(Cabin)
+  cabin := new(Cabin)
+  cabin.channels = make([] chan *Event, 0)
+  return cabin
+}
+
+func (cabin *Cabin) Subscribe(channel chan *Event) {
+  cabin.channels = append(cabin.channels, channel)
 }
 
 /* Log an object */
 func (cabin *Cabin) Log(object interface{}) {
-  type_ := reflect.TypeOf(object)
-  value := reflect.ValueOf(object)
+  event := &Event{Timestamp: time.UTC(), Object: object}
 
-  event := &Event{time.UTC(), &object}
-
-  /* Dereference reflected pointer values */
-  if type_.Kind() == reflect.Ptr {
-    type_ = type_.Elem()
+  for _, channel := range cabin.channels {
+    channel <- event
   }
-  if value.Kind() == reflect.Ptr {
-    value = value.Elem()
-  }
-
-  if type_.Kind() != reflect.Struct {
-    // this isn't a Struct
-    // TODO(sissel): Support this.
-    return
-  }
-
-  /* Print the timestamp */
-  fmt.Printf("%s: ", event.timestamp)
-
-  /* For every field in this object, emit the name, type, and current value */
-  for i := 0; i < type_.NumField(); i++ {
-    field := type_.Field(i)
-    if field.Anonymous {
-      continue;
-    }
-
-    switch field.Type.Kind() {
-      case reflect.Int:
-        fmt.Printf("%s(%s)=%d, ", field.Name, field.Type, value.Field(i).Int())
-      case reflect.Float32, reflect.Float64:
-        fmt.Printf("%s(%s)=%f, ", field.Name, field.Type, value.Field(i).Float())
-      case reflect.String:
-        fmt.Printf("%s(%s)=%s, ", field.Name, field.Type, value.Field(i).String())
-      case reflect.Interface:
-        fmt.Printf("%s(%s)=%s, ", field.Name, field.Type, value.Field(i).Interface())
-      case reflect.Bool:
-        fmt.Printf("%s(%s)=%s, ", field.Name, field.Type, value.Field(i).Bool())
-    }
-  }
-  fmt.Printf("\n")
 }
