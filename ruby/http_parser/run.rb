e985af65228a9a3d927c7a6e969c61444a37024d
Jordan Sissel <jls@semicomplete.com>
2012-03-03 23:22:24 -0800

    .

--- ruby/http_parser/run.rb
+++ ruby/http_parser/run.rb
@@ -1,13 +1,16 @@
 
 version = RUBY_VERSION
 platform = case RUBY_PLATFORM
-  when "java"; "jruby-JRUBY_VERSION"
-  else RUBY_PLATFORM
+  when "java"; "jruby-#{JRUBY_VERSION}"
+  else "ruby"
 end
 
-puts [version, platform].join(" @ ")
 
-file = [version,platform].join("-")
-output = File.new(file, "w")
+Dir.mkdir("output") unless File.directory?("output")
+file = [platform,version].join("@")
+output = File.new("output/#{file}", "w")
 $stdout.reopen(output)
+puts [version, platform].join(" @ ")
+puts "-" * 80
+puts
 load "./test.rb"
