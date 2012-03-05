a87d62fbf0ac13e4b1a8ed18f9043ba177ce959c
Jordan Sissel <jls@semicomplete.com>
2012-03-05 12:28:30 -0800

    This is what you thought you did.

--- style/example.rb
+++ style/example.rb
@@ -6,7 +6,7 @@ class Foo
   # arg1 - this is what arg1 is for and what it is expected to be
   #
   # This is what this method returns.
-  def foo(arg1, arg2)
+  def foo(arg1)
     # This is what you intended to for this segment of code
     this is what you actually did
   end
