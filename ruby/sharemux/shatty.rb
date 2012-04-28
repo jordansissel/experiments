bd57d3a039268d7b814e09af396dbcbe7d179f36
Jordan Sissel <jls@semicomplete.com>
2012-04-28 01:00:50 -0700

    - add readme

--- ruby/sharemux/shatty.rb
+++ ruby/sharemux/shatty.rb
@@ -14,9 +14,13 @@ class Shatty < Clamp::Command
 
       # binary mode.
       buffer = ""
-      buffer.force_encoding("BINARY")
+      #buffer.force_encoding("BINARY")
 
+      STDOUT.sync = true
       terminal, keyboard, pid = PTY.spawn(*command)
+
+      system("stty raw -echo") # yeah, perhaps we should use termios instead.
+      Thread.new { STDIN.each_char { |c| keyboard.syswrite(c) } }
       while true
         # Read from the terminal output and compute the time offset
         begin
@@ -31,12 +35,17 @@ class Shatty < Clamp::Command
 
         # for each chunk of text read from tmux, record
         # the timestamp (duration since 'start' of recording)
-        out.syswrite([time_offset.to_f, buffer.size, buffer].pack("GNA*"))
+        out.syswrite([time_offset.to_f, buffer.length, buffer].pack("GNA#{buffer.length}"))
+        $stdout.write(buffer)
       end
+
+      system("stty sane")
     end # def execute
   end # subcommand "record"
 
   subcommand "play", "Play a recording" do
+    parameter "[FILE]", "the file to play (same as --file)",
+      :attribute_name => :file
     option ["-f", "--file"], "FILE", "file to play",
       :default => "output.shatty"
 
@@ -44,23 +53,30 @@ class Shatty < Clamp::Command
       input = File.new(file, "r")
 
       buffer = ""
-      buffer.force_encoding("BINARY")
+      #buffer.force_encoding("BINARY")
       start = nil
 
       $stdout.sync = true
+      headersize = [1,1].pack("GN").size
+      last_time = 0
       while true
         # Read the header
-        input.sysread(16, buffer)
-        time_offset, length = buffer.unpack("GN")
-
-        if start.nil?
-          start = Time.at(time_offset)
+        begin
+          input.sysread(headersize, buffer)
+          #p input.pos => buffer
+          time, length = buffer.unpack("GN")
+          buffer = input.sysread(length, buffer)
+          #p input.pos => [ time_offset, length ]
+        rescue EOFError
+          break
         end
 
-        input.sysread(length, buffer)
-        $stdout.write(buffer)
+        # Sleep if necessary
+        sleep(time - last_time) if last_time > 0
+        last_time = time
 
-        sleep(time_offset - start.to_f)
+        # output this frame.
+        $stdout.syswrite(buffer)
       end
     end # def execute
   end # subcommand "play"
---
+++ unix/safely-crashing-reader/README.md
@@ -0,0 +1,73 @@
+# Safely crashing stdin consumer
+
+Let's say I wanted to replace 'logger' with a custom tool, but my custom tool
+was complex and could be prone to crashing. Perhaps this example, calling my
+custom tool 'slurper':
+
+```
+# Replace logger in this:
+./someprogram | logger -t myprogram
+
+# With this:
+./someprogram | slurper
+```
+
+If slurper crashes, its stdin is closed and 'someprogram' has its stdout
+brutally destroyed. The process usually dies via SIGPIPE or another read
+failure, like this example:
+
+```
+% ruby -e 'loop { puts "ok"; $stdout.flush }'  | true
+-e:1:in `flush': Broken pipe (Errno::EPIPE)
+```
+
+Since 'true' exits pretty quickly, ruby has its stdout blown up and the next
+write to stdout gets a SIGPIPE. Oops.
+
+Just like the 'true' command above, I worry that my hypothetical 'slurper'
+process could crash.
+
+## fork to the rescue
+
+We can solve this by having a master process babysit our 'slurper' using this
+pseudocode:
+
+```
+loop forever:
+  fork 'slurper'
+  wait for 'slurper' to die
+  exit if stdin is dead
+```
+
+## Example:
+
+See 'master.c' in this directory for the implementation of the above pseudocode.
+
+Here's what happens when we don't use the babysitting trick; stdin dies and we lose data:
+
+```
+% seq 10 | sh -c 'read a; echo "Read: $a"'
+Read: 1
+```
+
+We lost most of the output in our simulated crash. Oops.
+
+But what if we babysit it as described, restarting the dead 'stdin consumer'
+when it dies - continually, until stdin is actually closed?
+
+```
+% seq 10 | ./master sh -c 'read a; echo "Read: $a"'
+Read: 1
+Read: 2
+Read: 3
+Read: 4
+Read: 5
+Read: 6
+Read: 7
+Read: 8
+Read: 9
+Read: 10
+upstream stdin closed.
+```
+
+Boom! Victory.
