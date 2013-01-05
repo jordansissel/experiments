e3fe021022f9528688eda861fe93b22cda0c1fb6
Jordan Sissel <jls@semicomplete.com>
2013-01-04 16:05:41 -0800

    .

--- ruby/ssl/nonblock.rb
+++ ruby/ssl/nonblock.rb
@@ -2,17 +2,24 @@ require "openssl"
 require "socket"
 
 sslcontext = OpenSSL::SSL::SSLContext.new
-sslcontext.ssl_version = "TLSv1"
+sslcontext.ssl_version = :TLSv1
 
-# The OpenSSL docs seem to indicate that 'verify_callback'
-sslcontext.verify_mode = OpenSSL::SSL::VERIFY_NONE
+sslcontext.verify_mode = OpenSSL::SSL::VERIFY_PEER
+sslcontext.verify_callback = proc do |verified, context|
+  puts "Verify: #{verified} / #{context}"
+  verified
+end
+sslcontext.ssl_version = :TLSv1
+
 
 tcp = TCPSocket.new("twitter.com", 443)
 tls = OpenSSL::SSL::SSLSocket.new(tcp, sslcontext)
 begin
   tls.connect_nonblock
-rescue OpenSSL::SSL::SSLError => e
-  raise e if e.to_s !~ /read would block/
-  retry
+rescue IO::WaitReadable, IO::WaitReadable => e
+  raise e if e.to_s !~ /would block/
+  r,w,e = IO.select([tls], [tls], nil, nil)
+  $stdout.syswrite(".")
+  retry if r.size > 0 || w.size > 0
 end
 puts "connect_nonblock done"
