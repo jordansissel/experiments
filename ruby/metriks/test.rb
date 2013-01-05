aabd775587989bfce716dda787e0c248eaa6d145
Jordan Sissel <jls@semicomplete.com>
2013-01-04 16:05:54 -0800

    .

--- ruby/metriks/test.rb
+++ ruby/metriks/test.rb
@@ -1,15 +1,18 @@
 
 require "metriks"
-require "stud/benchmark"
+require "stud/interval"
 
 meter = Metriks.meter("hello")
+count = 0
 
-results = Stud::Benchmark.run(1000000) do
+include Stud
+interval(0.200) do
   meter.mark
+  count += 1
+  if count > 30
+    %w(count one_minute_rate five_minute_rate fifteen_minute_rate).each do |m|
+      puts m => meter.send(m.to_sym)
+    end
+    count = 0
+  end
 end
-
-puts "Duration: #{results.sum} - Rate: #{results.count / results.sum}"
-puts "Environment: #{results.environment}"
-puts "Breakdown:"
-results.pretty_print
-
