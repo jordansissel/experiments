068cae8b3be90db47c25da4beae9359e3331610d
Jordan Sissel <jls@semicomplete.com>
2013-01-04 16:04:33 -0800

    .

--- go/tls/conn.go
+++ go/tls/conn.go
@@ -29,6 +29,8 @@ func (l *Lumberjack) connect_once() (err error) {
   return
 }
 
+/* Connect to a remote lumberjack server. This blocks until the connection is
+ * ready. It will retry until successful. */
 func (l *Lumberjack) connect() {
   for !l.connected() {
     err := l.connect_once()
@@ -36,19 +38,27 @@ func (l *Lumberjack) connect() {
       fmt.Printf("Error connecting: %s\n", err)
     }
   }
-}
+} /* Lumberjack#connect */
 
+
+/* Disconnect from the lumberjack server */
 func (l *Lumberjack) disconnect() {
-
-}
+  if !l.connected() { return }
+  l.tls.Close()
+  l.conn.Close()
+  l.tls = nil
+  l.conn = nil
+} /* Lumberjack#disconnect */
 
 func (l *Lumberjack) publish(event map[string]string) {
   if !l.connected() { l.connect() }
+
+  writeData(l.tls, 1, event)
 }
 
 func main() {
   l := Lumberjack{Addresses: []string{"localhost:4000"}, CAPath: "/home/jls/projects/logstash/server.crt"}
 
-  l.connect()
-  fmt.Printf("Connected!\n");
+  l.publish(map[string]string{ "hello": "world"})
+  l.disconnect()
 }
--- go/tls/proto.go
+++ go/tls/proto.go
@@ -1,28 +1,25 @@
 package main
 
 import (
-  //"fmt"
   "encoding/binary"
-  //"os"
-  //"time"
   "io"
 )
 
-//func WriteData(:tabe
-  //conn.Write([]byte("1D"))
+func writeData(writable io.Writer, sequence uint32, m map[string]string) {
+  writable.Write([]byte("1D"))
+  binary.Write(writable, binary.BigEndian, uint32(sequence))
+  writeMap(writable, m)
+}
 
-  // Write the sequence
-  //binary.Write(conn, binary.BigEndian, sequence)
-
-func writeMap(conn io.Writer, sequence uint32, m map[string]string) {
-
+func writeMap(writable io.Writer, m map[string]string) {
   // How many fields in this data frame
-  binary.Write(conn, binary.BigEndian, uint32(len(m)))
+  binary.Write(writable, binary.BigEndian, uint32(len(m)))
 
   for key, value := range m {
-    binary.Write(conn, binary.BigEndian, uint32(len(key)))
-    conn.Write([]byte(key))
-    binary.Write(conn, binary.BigEndian, uint32(len(value)))
-    conn.Write([]byte(value))
+    binary.Write(writable, binary.BigEndian, uint32(len(key)))
+    writable.Write([]byte(key))
+
+    binary.Write(writable, binary.BigEndian, uint32(len(value)))
+    writable.Write([]byte(value))
   }
 } /* WriteMap */
