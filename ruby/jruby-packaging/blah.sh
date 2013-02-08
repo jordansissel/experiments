44d9e397eed1d1158f1fe9d1a964749e57ec4ab2
Jordan Sissel <jls@semicomplete.com>
2013-02-07 16:38:25 -0800

    .

--- ruby/jruby-packaging/blah.sh
+++ ruby/jruby-packaging/blah.sh
@@ -4,28 +4,6 @@ jar=example.jar
 echo "Copying jar"
 cp ~/build/jruby/lib/jruby-complete.jar $jar
 
-echo "Generating runner"
-cat > runner.rb <<RUBY
-
-def boomstick(path)
-  start = Time.now
-  begin
-    require(path)
-  rescue LoadError => e
-    puts e.class => e.to_s
-  end
-  duration = Time.now - start
-  puts "require(#{path}) took #{duration}"
-end
-
-boomstick("geoip") # this should succeed
-boomstick("geoip") # do it twice to time it
-boomstick("ftw") # another success
-boomstick("json") # another success
-boomstick("this-does-not-exist") # show failed load case
-boomstick("this-does-not-exist") # show failed load case x2
-RUBY
-
 echo "Adding gems to the jar"
 jar uf $jar \
   -C /home/jls/projects/logstash/vendor/bundle/jruby/1.9 gems \
@@ -43,3 +21,6 @@ jar ufe $jar runner
 
 #echo "Indexing jar"
 #jar i $jar
+
+echo "Running jar"
+java -jar $jar
--- ruby/jruby-packaging/runner.rb
+++ ruby/jruby-packaging/runner.rb
@@ -1,18 +1,22 @@
 
 def boomstick(path)
   start = Time.now
+  loaded_before = $LOADED_FEATURES.clone
   begin
     require(path)
   rescue LoadError => e
     puts e.class => e.to_s
   end
+  load_delta = $LOADED_FEATURES - loaded_before
   duration = Time.now - start
   puts "require(#{path}) took #{duration}"
+  puts "  files loaded: #{load_delta.count}"
 end
 
+boomstick("json") # should succeed
 boomstick("geoip") # this should succeed
+boomstick("this-does-not-exist") # show failed load case
 boomstick("geoip") # do it twice to time it
 boomstick("ftw") # another success
-boomstick("json") # another success
 boomstick("this-does-not-exist") # show failed load case
 boomstick("this-does-not-exist") # show failed load case x2
