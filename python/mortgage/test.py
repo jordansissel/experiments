8f994cb69d83a3ad079d213c63d48c75975dd835
Jordan Sissel <jls@semicomplete.com>
2013-02-12 21:29:58 -0800

    .

--- python/mortgage/test.py
+++ python/mortgage/test.py
@@ -1,5 +1,4 @@
 import random
-import matplotlib.pyplot as plt
 
 class Payment(object):
   def __init__(self, interest, principle):
@@ -48,12 +47,28 @@ class Option1Secondary(Mortgage):
       # Current prime rate is 3.25%, so minimum is 5%
       # Allow +2% for fluctuation in the future.
       self.current_rate = 0.05 + (random.random() * 0.02)
-      print self.current_rate
     return self.current_rate
 
+class Option2Primary(Mortgage):
+  rate = 0.035
+  amount = 617000 * 1.0175 # 1.75% UFPMI for FHA loan
+  fees = 0
+  term = 30
+  fixed = True
+    
+  @property
+  def minimum_payment(self):
+    if self.month < (5 * 12): # first 5 years requires PMI
+      return 2777.72 + 643.22
+    else:
+      return 2777.72
+      
 
-fig = plt.figure()
+#print "interest1,principle1,interest2,principle2"
+#for p1, p2 in zip(Option1Primary().payments(), Option1Secondary().payments()):
+  #print "%f,%f,%f,%f" % (p1.interest, p1.principle, p2.interest, p2.principle)
 
-o = Option1Secondary()
-for p in o.payments():
-  print repr(p)
+print "interest,principle"
+for p in Option2Primary().payments():
+  print "%f,%f" % (p.interest, p.principle)
+
