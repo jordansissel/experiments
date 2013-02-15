4984c84c4ce870c3755e8279a46fc63b780ed344
Jordan Sissel <jls@semicomplete.com>
2013-02-15 00:19:25 -0800

    - test URI.parse speeds (which are terrible) against my own bullshit hacks

---
+++ ruby/uri/test.rb
@@ -0,0 +1,20 @@
+require "uri"
+
+uri_re = /^([A-z+-]+):\/\/([^A-z0-9_.]+)(?::[0-9]+)?(\/.*)/
+
+url = "lumberjack://foobar/var/log/messages"
+
+def time(iterations,&block)
+  start = Time.now
+  iterations.times do
+    block.call
+  end
+  return Time.now - start
+end
+
+iterations = 1000000
+2.times do
+  p "URI.parse" => time(iterations) { URI.parse(url) }
+  p "regexp" => time(iterations) { uri_re.match(url) }
+end
+
