a2cee4d86eb88618ccc51befcb9872fc53387124
Jordan Sissel <jls@semicomplete.com>
2013-04-08 18:29:17 -0700

    - linetime

--- ruby/start-time/linetime.rb
+++ ruby/start-time/linetime.rb
@@ -4,8 +4,11 @@
 
 io = IO.popen(ARGV)
 
+log = File.new("log", "w+")
+
 start = Time.now
 io.each do |line|
   puts "%.06f %s" % [Time.now - start, line.chomp]
+  log.puts "%.06f %s" % [Time.now - start, line.chomp]
 end
 
