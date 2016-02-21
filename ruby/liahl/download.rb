0bcfd7471cc9f5c02dba0a009c5770de18f24e4e
Jordan Sissel <jls@semicomplete.com>
2016-02-20 19:33:02 -0800

    Move to clamp

--- ruby/liahl/download.rb
+++ ruby/liahl/download.rb
@@ -3,51 +3,61 @@ require "open-uri"
 require "elasticsearch"
 require "digest"
 require "time"
+require "clamp"
 
-def index(url, content)
-  es = Elasticsearch::Client.new
-  es.index(index: "hockey-scrape", type: "web", 
-    id: Digest::SHA256.hexdigest(url),
-    body: {
-      fetched_at: Time.now.iso8601(3),
-      url: url,
-      content: content
-    })
-end
+class LIAHLDownload < Clamp::Command
+  option "--elasticsearch-url", "URL", "Elasticsearch url", required: true
+  option "--elasticsearch-index", "INDEX", "Elasticsearch index", default: "hockey-scrape"
 
-def process_season(season)
-  puts "Fetching season #{season}"
-  base_url = "http://stats.liahl.org"
-  stats_main = "#{base_url}/display-stats.php?league=1&season=#{season}"
-  dir = "seasons/#{season}"
-
-  id = Digest::SHA256.hexdigest(stats_main)
-
-  content = open(stats_main).read.force_encoding(Encoding::UTF_8).encode(Encoding::UTF_8, :invalid => :replace, :undef => :replace)
-  index(stats_main, content)
-
-  # Parse the player stats links
-  doc = Nokogiri::HTML(content)
-  stats_links = doc.xpath("//a[contains(text(),'Player Stats')]")
-  levels = doc.xpath("//a[contains(text(), 'Player Stats')]/../../preceding-sibling::tr[1]").collect(&:text)
-
-  stats_links.each_with_index do |link|
-    level = levels.shift
-    href = link.attributes["href"].value
-    url = base_url + "/" + href
-    puts url
-    uri = URI.parse(url)
-    division_stats_content = open(url).read.force_encoding(Encoding::UTF_8).encode(Encoding::UTF_8, :invalid => :replace, :undef => :replace)
-    query = CGI.parse(URI.parse(url).query)
-    index(url, division_stats_content)
-    sleep(0.500)
+  def index(url, content)
+    es = Elasticsearch::Client.new(host: elasticsearch_url)
+    es.index(index: elasticsearch_index, type: "web", 
+      id: Digest::SHA256.hexdigest(url),
+      body: {
+        fetched_at: Time.now.iso8601(3),
+        url: url,
+        content: content
+      })
   end
-rescue => e
-  puts "Failure fetching season #{season}: #{e}"
-  raise
+
+  def process_season(season)
+    puts "Fetching season #{season}"
+    base_url = "http://stats.liahl.org"
+    stats_main = "#{base_url}/display-stats.php?league=1&season=#{season}"
+    dir = "seasons/#{season}"
+
+    id = Digest::SHA256.hexdigest(stats_main)
+
+    content = open(stats_main).read.force_encoding(Encoding::UTF_8).encode(Encoding::UTF_8, :invalid => :replace, :undef => :replace)
+    index(stats_main, content)
+
+    # Parse the player stats links
+    doc = Nokogiri::HTML(content)
+    stats_links = doc.xpath("//a[contains(text(),'Player Stats')]")
+    levels = doc.xpath("//a[contains(text(), 'Player Stats')]/../../preceding-sibling::tr[1]").collect(&:text)
+
+    stats_links.each_with_index do |link|
+      level = levels.shift
+      href = link.attributes["href"].value
+      url = base_url + "/" + href
+      puts url
+      uri = URI.parse(url)
+      division_stats_content = open(url).read.force_encoding(Encoding::UTF_8).encode(Encoding::UTF_8, :invalid => :replace, :undef => :replace)
+      query = CGI.parse(URI.parse(url).query)
+      index(url, division_stats_content)
+      sleep(0.500)
+    end
+  rescue => e
+    puts "Failure fetching season #{season}: #{e}"
+    raise
+  end
+
+  def execute
+    seasons = (1..33)
+    seasons.each do |s| 
+      process_season(s)
+    end
+  end
 end
 
-seasons = (1..33)
-seasons.each do |s| 
-  process_season(s)
-end
+LIAHLDownload.run(ARGV)
