b0f0ea49438f0517c2ed800d26a2386d3567b75f
Jordan Sissel <jls@semicomplete.com>
2016-03-16 17:44:45 -0700

    Uuugh.
    Benchmark.bmbm will collect all the report blocks first, then execute
    them in order at the end of the bmbm call.
    
    This gave me a false report because the report name was generated _during_ the
    bmbm call, but each actual report is executed at the end of the bmbm block.
    
    Because I had an outer scope channel and setting that channel's level within
    the `bmbm` call, this would cause the reports to correctly show  flipping
    between true and false, but the actual benchmark would always have the level
    set to debug even when the title was set to info.
    
    Avoiding setting the level within the test will help.
    
                                                             user     system      total        real
    DefCaller info: debug? (false)                       0.030000   0.000000   0.030000 (  0.037523)
    DefCaller info: debug? && debug() (false)            0.040000   0.000000   0.040000 (  0.039549)
    DefCaller info: debug? && rand(100) (false)          0.040000   0.000000   0.040000 (  0.037984)
    DefCaller info: debug? && nil (false)                0.040000   0.000000   0.040000 (  0.037853)
    DefCaller info: if debug? ... debug() (false)        0.040000   0.000000   0.040000 (  0.037591)
    Cabin::Channel debug: debug? (true)                  0.090000   0.000000   0.090000 (  0.099390)
    Cabin::Channel debug: debug? && debug() (true)       9.540000   0.690000  10.230000 ( 10.224737)
    Cabin::Channel debug: debug? && rand(100) (true)     0.150000   0.000000   0.150000 (  0.156217)
    Cabin::Channel debug: debug? && nil (true)           0.110000   0.000000   0.110000 (  0.109088)
    Cabin::Channel debug: if debug? ... debug() (true)   9.790000   0.710000  10.500000 ( 10.493780)
    Cabin::Channel info: debug? (false)                  0.090000   0.000000   0.090000 (  0.093102)
    Cabin::Channel info: debug? && debug() (false)       0.100000   0.000000   0.100000 (  0.098334)
    Cabin::Channel info: debug? && rand(100) (false)     0.090000   0.000000   0.090000 (  0.093859)
    Cabin::Channel info: debug? && nil (false)           0.100000   0.000000   0.100000 (  0.101345)
    Cabin::Channel info: if debug? ... debug() (false)   0.100000   0.000000   0.100000 (  0.095535)

--- ruby/weird-conditional-timing/bench.rb
+++ ruby/weird-conditional-timing/bench.rb
@@ -9,12 +9,11 @@ DEBUG = LEVELS[:debug]
 
 class B
   def initialize
+    @level = :info
     @buffer = ""
   end
 
-  def level=(value)
-    @level = value
-  end
+  attr_accessor :level
 end
 
 class Def < B
@@ -57,64 +56,59 @@ class DefineMethod < B
   end
 end
 
-iterations = 1_000_000
+iterations = 500_000
 
-debug_channel = Cabin::Channel.new
-debug_channel.level = :debug
-info_channel = Cabin::Channel.new
-info_channel.level = :info
 #objects = [debug_channel, info_channel, Def.new, DefineMethod.new]
 #objects = [debug_channel, info_channel]
-objects = [Def.new, DefineMethod.new, DefCaller.new]
+#objects = [Def.new, DefineMethod.new, DefCaller.new]
+objects = [DefCaller.new, Cabin::Channel.new.tap { |c| c.level = :debug }, Cabin::Channel.new.tap { |c| c.level = :info }]
 
 msg = "ok"
-Benchmark.bmbm(30) do |x|
+Benchmark.bm(50) do |x|
   objects.each do |c|
-    [:info, :debug].each do |level|
-      c.level = level
-      prefix = "#{c.class.name} #{level}:"
-      x.report("#{prefix} debug? (#{c.debug?})") do
-        iterations.times { c.debug? }
-      end
-      x.report("#{prefix} debug? && debug() (#{c.debug?})") do
-        iterations.times { c.debug? && c.debug(msg) }
-      end
-      x.report("#{prefix} debug? && rand(100) (#{c.debug?})") do
-        iterations.times { c.debug? && rand(100) }
-      end
-      x.report("#{prefix} debug? && nil (#{c.debug?})") do
-        iterations.times { c.debug? && nil }
-      end
-      x.report("#{prefix} if debug? ... debug() (#{c.debug?})") do
-        iterations.times { if c.debug? ; c.debug(msg); end }
-      end
+    level = c.level
+    prefix = "#{c.class.name} #{level}:"
+    x.report("#{prefix} debug? (#{c.debug?})") do
+      iterations.times { c.debug? }
+    end
+    x.report("#{prefix} debug? && debug() (#{c.debug?})") do
+      iterations.times { c.debug? && c.debug(msg) }
+    end
+    x.report("#{prefix} debug? && rand(100) (#{c.debug?})") do
+      iterations.times { c.debug? && rand(100) }
+    end
+    x.report("#{prefix} debug? && nil (#{c.debug?})") do
+      iterations.times { c.debug? && nil }
+    end
+    x.report("#{prefix} if debug? ... debug() (#{c.debug?})") do
+      iterations.times { if c.debug? ; c.debug(msg); end }
+    end
 
-      if ENV["DEBUG"]
-        x.report("#{prefix} debug() (#{c.debug?})") do
-          iterations.times { c.debug(msg) }
-        end
-        x.report("#{prefix} nothing") do
-          iterations.times { }
-        end
-        x.report("#{prefix} x = debug? (#{c.debug?})") do
-          iterations.times { x = c.debug? }
-        end
-        x.report("#{prefix} false && debug() (#{c.debug?})") do
-          iterations.times { false && c.debug(msg) }
-        end
-        x.report("#{prefix} true && debug() (#{c.debug?})") do
-          iterations.times { true && c.debug(msg) }
-        end
-        x.report("#{prefix} value && debug() (#{c.debug?})") do
-          value = c.debug?
-          iterations.times { value && c.debug(msg) }
-        end
-        x.report("#{prefix} debug? and debug() (#{c.debug?})") do
-          iterations.times { c.debug? and c.debug(msg) }
-        end
-        x.report("#{prefix} debug() if debug?(#{c.debug?})") do
-          iterations.times { c.debug(msg) if c.debug? }
-        end
+    if ENV["DEBUG"]
+      x.report("#{prefix} debug() (#{c.debug?})") do
+        iterations.times { c.debug(msg) }
+      end
+      x.report("#{prefix} nothing") do
+        iterations.times { }
+      end
+      x.report("#{prefix} x = debug? (#{c.debug?})") do
+        iterations.times { x = c.debug? }
+      end
+      x.report("#{prefix} false && debug() (#{c.debug?})") do
+        iterations.times { false && c.debug(msg) }
+      end
+      x.report("#{prefix} true && debug() (#{c.debug?})") do
+        iterations.times { true && c.debug(msg) }
+      end
+      x.report("#{prefix} value && debug() (#{c.debug?})") do
+        value = c.debug?
+        iterations.times { value && c.debug(msg) }
+      end
+      x.report("#{prefix} debug? and debug() (#{c.debug?})") do
+        iterations.times { c.debug? and c.debug(msg) }
+      end
+      x.report("#{prefix} debug() if debug?(#{c.debug?})") do
+        iterations.times { c.debug(msg) if c.debug? }
       end
     end
   end
