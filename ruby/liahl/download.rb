69f0879cad900fce55fd29ce80438df42149187d
Jordan Sissel <jls@semicomplete.com>
2016-06-27 20:57:31 -0700

    WIP

--- ruby/liahl/download.rb
+++ ruby/liahl/download.rb
@@ -6,54 +6,95 @@ require "time"
 require "clamp"
 
 class LIAHLDownload < Clamp::Command
+  HTTP_HEADERS = {
+      "User-Agent" => "liahl stats fetcher (Contact me jls@semicomplete.com if this script is causing problems)"
+  }
+
   option "--elasticsearch-url", "URL", "Elasticsearch url", required: true
   option "--elasticsearch-index", "INDEX", "Elasticsearch index", default: "hockey-scrape"
 
-  def index(url, content)
+  def time(text, &block)
+    start = Time.now
+    value = block.call
+    duration = Time.now - start
+    puts(format("time> %.2f | %s", duration, text))
+    value
+  end
+
+  def open(*args)
+    sleep(0.5)
+    time("fetch #{args.first}") do
+      Kernel::open(*args)
+    end
+  end
+
+  def index(url)
     es = Elasticsearch::Client.new(host: elasticsearch_url)
-    es.index(index: elasticsearch_index, type: "web", 
-      id: Digest::SHA256.hexdigest(url),
-      body: {
-        fetched_at: Time.now.iso8601(3),
-        url: url,
-        content: content
-      })
+    document_id = Digest::SHA256.hexdigest(url)
+
+    begin
+      document = time("cache check | #{url}") do
+        es.get(index: elasticsearch_index, id: document_id)
+      end
+      document["_source"]["content"]
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound
+      content = open(url, HTTP_HEADERS).read.force_encoding(Encoding::UTF_8).encode(Encoding::UTF_8, :invalid => :replace, :undef => :replace)
+      es.index(index: elasticsearch_index,
+               type: "web", 
+               id: document_id,
+               body: {
+                 fetched_at: Time.now.iso8601(3),
+                 url: url,
+                 content: content
+               })
+      content
+    end
   end
 
   def process_season(season)
     puts "Fetching season #{season}"
     base_url = "http://stats.liahl.org"
     stats_main = "#{base_url}/display-stats.php?league=1&season=#{season}"
-    dir = "seasons/#{season}"
 
     id = Digest::SHA256.hexdigest(stats_main)
 
-    content = open(stats_main).read.force_encoding(Encoding::UTF_8).encode(Encoding::UTF_8, :invalid => :replace, :undef => :replace)
-    index(stats_main, content)
+    content = index(stats_main)
 
     # Parse the player stats links
     doc = Nokogiri::HTML(content)
     stats_links = doc.xpath("//a[contains(text(),'Player Stats')]")
     levels = doc.xpath("//a[contains(text(), 'Player Stats')]/../../preceding-sibling::tr[1]").collect(&:text)
 
-    stats_links.each_with_index do |link|
+    false && stats_links.each_with_index do |link|
       level = levels.shift
       href = link.attributes["href"].value
       url = base_url + "/" + href
-      puts url
-      uri = URI.parse(url)
-      division_stats_content = open(url).read.force_encoding(Encoding::UTF_8).encode(Encoding::UTF_8, :invalid => :replace, :undef => :replace)
-      query = CGI.parse(URI.parse(url).query)
-      index(url, division_stats_content)
-      sleep(0.500)
+      index(url)
     end
+
+    # Download all the current score sheets
+    doc.xpath("//a[contains(@href, 'display-schedule.php')]").each do |link|
+      team = link.text
+      href = link.attributes["href"].value
+      url = base_url + "/" + href
+      schedule_content = index(url)
+
+      schedule = Nokogiri::HTML(schedule_content)
+      schedule.xpath("//a[contains(@href,'oss-scoresheet')]").each do |game_link|
+        game_href = game_link.attributes["href"].value
+        game_url = base_url + "/" + game_href
+        puts game_url
+        index(game_url)
+      end
+    end
+
   rescue => e
     puts "Failure fetching season #{season}: #{e}"
     raise
   end
 
   def execute
-    seasons = (1..33)
+    seasons = (35..35)
     seasons.each do |s| 
       process_season(s)
     end
--- ruby/liahl/stats.rb
+++ ruby/liahl/stats.rb
@@ -1,14 +1,61 @@
 require "nokogiri"
 require "elasticsearch"
+require "peach"
 require "clamp"
 
+def logtime(message, &block)
+  start = Time.now
+  block.call
+  duration = Time.now - start
+  puts format("%.2fs: %s", duration, message)
+end
+
+class Player
+  def initialize(number, position, name)
+    @number, @position, @name = number, position, name
+  end
+
+  def to_hash
+    {
+      :number => @number,
+      :position => @position,
+      :name => @name
+    }
+  end
+end
+
+class Point
+  def initialize(goal, assist, time, period)
+    @goal, @assist, @time, @period = goal, assist, time, period
+  end
+
+  def to_hash
+    {
+      goal: @goal,
+      assist: @assist,
+      time: @time,
+      period: @period
+    }
+  end
+end
+
 class Stats < Clamp::Command
+  module XPath
+    SHOTS_ON_GOAL = "//th[contains(text(), 'Shots on Goal')]/ancestor::table[1]/tr/th"
+    PLAYERS = "//th[contains(text(), 'Players in')]/ancestor::table[1]"
+    REFEREES = "//th[contains(text(), 'Referee')]/following-sibling::td"
+    SCOREKEEP = "//th[contains(text(), 'Scorekeeper')]/following-sibling::td"
+    EVENTS = "/html/body/table[last()]/tr[last()]"
+  end
+
   option "--elasticsearch-url", "URL", "Elasticsearch url", required: true
+  option "--elasticsearch-index", "INDEX", "Elasticsearch index", default: "hockey-scrape"
 
   def fetch(url)
     es = Elasticsearch::Client.new(host: elasticsearch_url)
     id = Digest::SHA256.hexdigest(url)
-    result = es.get(index: "hockey-scrape", id: Digest::SHA256.hexdigest(url))
+    p url => id
+    result = es.get(index:elasticsearch_index, id: id)
     result["_source"]["content"]
   end
 
@@ -17,25 +64,85 @@ class Stats < Clamp::Command
     base_url = "http://stats.liahl.org"
     puts "Fetching season #{season}"
     stats_main = "#{base_url}/display-stats.php?league=1&season=#{season}"
+    puts stats_main
 
     doc = Nokogiri::HTML(fetch(stats_main))
+
+    #process_stats(es, base_url, doc)
+    process_games(es, base_url, doc)
+  end
+
+  def process_games(es, base_url, doc)
+    team_links = doc.xpath("//a[contains(@href,'display-schedule.php')]")
+
+    team_links.each do |team_link|
+      url = base_url + "/" + team_link.attributes["href"]
+      schedule = Nokogiri::HTML(fetch(url))
+
+      schedule.xpath("//a[contains(@href,'oss-scoresheet')]").each do |game_link|
+        game_href = game_link.attributes["href"].value
+        game_url = base_url + "/" + game_href
+        game = Nokogiri::HTML(fetch(game_url))
+
+        level = game.xpath("//td[contains(text(),'Level:')]").first.text.sub(/^Level:\s*/, "")
+
+        visitor_players, home_players = game.xpath(XPath::PLAYERS).collect { |table| parse_players(table) }
+
+        visitor = {
+          players: visitor_players,
+          shots: game.xpath(XPath::SHOTS_ON_GOAL)[1].text.sub(/Visitor:/, "").to_i,
+        }
+
+        home = {
+          players: home_players,
+          shots: game.xpath(XPath::SHOTS_ON_GOAL)[2].text.sub(/Home:/, "").to_i,
+        }
+
+        refs = game.xpath(XPath::REFEREES).collect(&:text)
+        scorekeep = game.xpath(XPath::SCOREKEEP).first.text
+
+        visitor_scoring, home_scoring = game.xpath("//th[text() = 'Scoring']").collect { |table| parse_scoring(table) }
+        visitor_penalties, home_penalties = game.xpath("//th[text() = 'Penalties']").collect { |table| parse_penalties(table) }
+
+        require "pry"
+        binding.pry
+      end
+    end
+  end
+
+  def parse_players(doc)
+    doc.xpath("./tr/td/table/tr/td").each_slice(3).collect do |number, position, name|
+      Player.new(number.text.to_i, position.text.strip, name.text.strip)
+    end
+  end
+
+  def parse_scoring(doc)
+    doc.xpath("../../tr/td").each_slice(6).collect do |period, time, condition, goal, *assist|
+      p [period.text.to_i, time.text, condition.text, goal.text.to_i, assist.collect(&:text).collect(&:to_i)]
+    end
+  end
+
+  def parse_penalties(doc)
+    doc.xpath("../../tr/td").each_slice(6).collect do |period, number, infraction, minutes, off_ice, pstart, pend, on_ice|
+      p [period.text.to_i, number.text.to_i, infraction.text, minutes.text.to_i, off_ice, pstart.text, pend.text, on_ice]
+    end
+  end
+
+  def process_stats(es, base_url, doc)
     stats_links = doc.xpath("//a[contains(text(),'Player Stats')]")
 
     levels = doc.xpath("//a[contains(text(), 'Player Stats')]/../../preceding-sibling::tr[1]").collect(&:text)
 
     player_stats = {}
     goalie_stats = {}
-    stats_links.each_with_index do |link|
+    stats_links.peach(2) do |link|
       level = levels.shift
       href = link.attributes["href"].value
-      $stdout.write("#{level}: ")
-      $stdout.flush
 
       url = base_url + "/" + href
       division_stats = Nokogiri::HTML(fetch(url))
       season = href[/season=\d+/].split("=").last
       level_num = href[/level=\d+/].split("=").last
-
 
       player_entries = division_stats.xpath("//table[contains(*//th/text(), 'Player Stats')]//tr[td]")
       bulk = []
@@ -49,8 +156,10 @@ class Stats < Clamp::Command
         goals = cells[4].text
         assists = cells[5].text
         penalty_time = cells[6].text
-        player_stats["#{name}:#{number}"] = entry = {
-          id: [name, number, team, season].join(","),
+        id = [name, number, team, season].join(",")
+        puts(format("[%d]%s - %s", number, name, team)) if team =~ /destroyer/i
+        player_stats[id] = entry = {
+          id: id,
           name: name,
           number: number.to_i,
           team: team,
@@ -64,10 +173,7 @@ class Stats < Clamp::Command
           points: goals.to_i + assists.to_i,
           position: "skater"
         }
-        p entry
         bulk += [ { index: { _index: "hockey", _type: "humans", _id: entry[:id] } }, entry ]
-        $stdout.write("P")
-        $stdout.flush
       end
 
       goalie_entries = division_stats.xpath("//table[contains(*//th/text(), 'Goalie Stats')]//tr[td]")
@@ -91,19 +197,19 @@ class Stats < Clamp::Command
           season: season,
           position: "goalie",
         }
-        p entry
         bulk += [ { index: { _index: "hockey", _type: "humans", _id: entry[:id] } }, entry ]
-        $stdout.write("G")
-        $stdout.flush
       end
       next if bulk.empty?
-      es.bulk(body: bulk)
-      $stdout.puts
+
+      logtime("Season #{season} / Level #{level}") do
+        es.bulk(body: bulk)
+      end
     end
   end
 
   def execute
-    seasons = (1..33)
+    #seasons = (30..35)
+    seasons = (35..35)
     seasons.each do |s| 
       process_season(s)
     end
