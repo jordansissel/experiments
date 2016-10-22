092a986626b6faf34642f92bb357ff4a92f97b53
Jordan Sissel <jls@semicomplete.com>
2016-10-22 10:34:23 -0700

    WIP fiddling with function selection checking

--- rust/rpi/src/main.rs
+++ rust/rpi/src/main.rs
@@ -29,6 +29,29 @@ fn main() {
 
   println!("Address: {:?}", address);
 
+  // 0x7E200000 - 0x7E200014 are 32-bit parts with 3 bits each representing the configuration ("function select") of each GPIO pin.
+  unsafe {
+    for fsel in 0..6 {
+      let fselread = *addr.offset(0x0000 + 4*fsel);
+      for pin in 0..10 {
+        let num = fsel * 10 + pin;
+        let value = (fselread >> (pin * 3)) & 7;
+        match value {
+          0 /* 000 */ => println!("Pin {} is an input", num),
+          1 /* 001 */ => println!("Pin {} is an input", num),
+          2 /* 010 */ => println!("Pin {} is function5", num),
+          3 /* 011 */ => println!("Pin {} is function4", num),
+          4 /* 100 */ => println!("Pin {} is function0", num),
+          5 /* 101 */ => println!("Pin {} is function1", num),
+          6 /* 110 */ => println!("Pin {} is function2", num),
+          7 /* 111 */ => println!("Pin {} is function3", num),
+          _ => println!("Pin {} unknown: {:.3b}", num, value)
+        }
+      }
+    }
+  }
+
+
   // 0x7e20 should be the where `/dev/gpiomem` dumps us, so everything is relative to that.
   // 0x7e200034 and 0x7e200038 are both 32-bits (each) containing pin levels.
   unsafe {
