a335b8cb057bd1acf83b501593b2a96926986349
Jordan Sissel <jls@semicomplete.com>
2016-10-24 07:20:20 +0000

    Fiddling more with rust and raspberry pi gpio

--- rust/rpi/src/main.rs
+++ rust/rpi/src/main.rs
@@ -1,63 +1,18 @@
-//use std::io::prelude::*;
-//use std::os::ext::io::AsRawFd;
-use std::os::unix::io::AsRawFd;
-use std::fs::OpenOptions;
+use std::{time, thread};
 extern crate libc;
+mod rpi;
 
+use rpi::{GPIO, Mode, Value};
 
 fn main() {
-  println!("Hello, world!");
+  let gpio = GPIO::new();
 
-  let devmem = match OpenOptions::new().write(true).read(true).open("/dev/gpiomem") {
-    Ok(s) => s,
-    Err(e) => { 
-      println!("Failed: {:?}", e);
-      return;
-    }
-  };
+  let pin = gpio.setup(21, Mode::Output);
 
-  let memfd = devmem.as_raw_fd();
-  let address: *mut libc::c_void;
-  let c_null = 0 as *mut libc::c_void;
-
-  unsafe {
-    address = libc::mmap(c_null, 4096, libc::PROT_READ | libc::PROT_WRITE, libc::MAP_SHARED, memfd, 0);
-  }
-
-  // BCM2835 docs: "All accesses are assumed to be 32bit"
-  let mut addr = address as *mut u32;
-
-  println!("Address: {:?}", address);
-
-  // 0x7E200000 - 0x7E200014 are 32-bit parts with 3 bits each representing the configuration ("function select") of each GPIO pin.
-  unsafe {
-    for fsel in 0..6 {
-      let fselread = *addr.offset(0x0000 + 4*fsel);
-      for pin in 0..10 {
-        let num = fsel * 10 + pin;
-        let value = (fselread >> (pin * 3)) & 7;
-        match value {
-          0 /* 000 */ => println!("Pin {} is an input", num),
-          1 /* 001 */ => println!("Pin {} is an input", num),
-          2 /* 010 */ => println!("Pin {} is function5", num),
-          3 /* 011 */ => println!("Pin {} is function4", num),
-          4 /* 100 */ => println!("Pin {} is function0", num),
-          5 /* 101 */ => println!("Pin {} is function1", num),
-          6 /* 110 */ => println!("Pin {} is function2", num),
-          7 /* 111 */ => println!("Pin {} is function3", num),
-          _ => println!("Pin {} unknown: {:.3b}", num, value)
-        }
-      }
-    }
-  }
-
-
-  // 0x7e20 should be the where `/dev/gpiomem` dumps us, so everything is relative to that.
-  // 0x7e200034 and 0x7e200038 are both 32-bits (each) containing pin levels.
-  unsafe {
-    let pinset1 = *(addr.offset(0x34/4));
-    println!("0..32: {:32.0b}", pinset1);
-    let pinset2 = *(addr.offset(0x38/4));
-    println!("33..64: {:32.0b}", pinset2);
+  loop {
+    pin.set();
+    thread::sleep(time::Duration::new(1,0));
+    pin.clear();
+    thread::sleep(time::Duration::new(1,0));
   }
 }
---
+++ rust/rpi/src/rpi/mod.rs
@@ -0,0 +1,132 @@
+extern crate libc;
+use std::os::unix::io::AsRawFd;
+use std::fs::OpenOptions;
+
+pub struct GPIO {
+  address: *mut libc::c_void
+}
+
+//#[derive(Debug)]
+//pub enum Pull {
+  //Up,
+  //Down,
+//}
+
+#[derive(Debug)]
+pub enum Mode {
+    //Input(Pull),
+    Output,
+}
+
+const GPIO_PIN_SET: u32 = 0x001C;
+const GPIO_PIN_CLEAR: u32 = 0x0028;
+
+impl GPIO {
+  pub fn new() -> GPIO {
+    let devmem = match OpenOptions::new().write(true).read(true).open("/dev/gpiomem") {
+      Ok(s) => s,
+      Err(e) => { 
+        panic!("Failed: {:?}", e);
+      }
+    };
+
+    let c_null = 0 as *mut libc::c_void;
+
+    let mmap: *mut libc::c_void;
+
+    unsafe {
+      mmap = libc::mmap(c_null, 4096, libc::PROT_READ | libc::PROT_WRITE,
+                             libc::MAP_SHARED, devmem.as_raw_fd(), 0);
+    }
+
+    return GPIO{address: mmap};
+  }
+
+  fn u32offset8(&self, offset: u32) -> *mut u32 {
+    unsafe {
+      return self.address.offset(offset as isize) as *mut u32;
+    }
+  }
+
+  // TODO(sissel): Return a result
+  pub fn setup(&self, pin: u32, mode: Mode) -> Pin {
+    match mode {
+      Mode::Output => {
+        let output = Pin::with_gpio(pin, self);
+        output.setup();
+        output
+      }
+    }
+  }
+
+  pub fn output(&self, pin: u32, value: Value) {
+    let base = match value {
+      Value::High => GPIO_PIN_SET,
+      Value::Low => GPIO_PIN_CLEAR,
+    };
+
+    unsafe {
+      let address = self.u32offset8(base).offset((pin / 32) as isize);
+      *address = 1 << pin;
+    }
+  }
+}
+
+pub struct Pin<'a> {
+  pin: u32,
+  gpio: &'a GPIO,
+  set_address: *mut u32,
+  clear_address: *mut u32,
+  pin_bit: u32,
+}
+
+impl<'a> Pin<'a> {
+  pub fn with_gpio(pin: u32, gpio: &'a GPIO) -> Pin<'a> {
+    let s: *mut u32; 
+    let c: *mut u32; 
+
+    unsafe {
+      s = gpio.u32offset8(GPIO_PIN_SET).offset((pin / 32) as isize);
+      c = gpio.u32offset8(GPIO_PIN_CLEAR).offset((pin / 32) as isize);
+    }
+
+    Pin{
+      pin: pin,
+      gpio: gpio,
+      set_address: s,
+      clear_address: c,
+      pin_bit: 1 << pin
+    }
+  }
+
+  pub fn setup(&self) {
+    // Each pin function selection is 3 bits wide.
+    // For 32 bits, this gives us 10 pins.
+    // ^^ BCM2835 ARM Peripherals 6.1
+    let offset = self.pin / 10;
+    let shift = 3 * (self.pin % 10);
+    let gpfsel = self.gpio.u32offset8(offset);
+
+    unsafe {
+      // Output mode is binary 001 
+      *gpfsel = (*gpfsel & !(0b111 << shift)) | (0b001 << shift);
+    }
+  }
+
+  pub fn set(&self) {
+    println!("set: {:x} {:b}", (self.set_address as usize) - (self.gpio.address as usize), self.pin_bit);
+    unsafe { *self.set_address = self.pin_bit }
+  }
+
+  pub fn clear(&self) {
+    println!("clear: {:x} {:b}", (self.clear_address as usize) - (self.gpio.address as usize), self.pin_bit);
+    unsafe { *self.clear_address = self.pin_bit }
+  }
+}
+
+#[derive(Debug)]
+pub enum Value {
+  High,
+  Low,
+}
+
