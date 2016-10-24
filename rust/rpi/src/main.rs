e517b0e3f2a28c16c61eef85dc841108488c7800
Jordan Sissel <jls@semicomplete.com>
2016-10-24 07:34:29 +0000

    software pwm for having an led fade in and out. Translated from some really old code I had for a soekris net4801's led. -- http://www.semicomplete.com/files/blogposts/20080106/glow.c

--- rust/rpi/src/main.rs
+++ rust/rpi/src/main.rs
@@ -9,10 +9,26 @@ fn main() {
 
   let pin = gpio.setup(21, Mode::Output);
 
+  let interval = 10000000;
+  let step = 100000;
+
   loop {
-    pin.set();
-    thread::sleep(time::Duration::new(1,0));
-    pin.clear();
-    thread::sleep(time::Duration::new(1,0));
+    let mut duty_cycle = 0;
+    while duty_cycle < interval {
+      duty_cycle += step;
+      pin.set();
+      thread::sleep(time::Duration::new(0,duty_cycle));
+      pin.clear();
+      thread::sleep(time::Duration::new(0,interval - duty_cycle));
+    }
+
+    duty_cycle = interval;
+    while duty_cycle > 0 {
+      duty_cycle -= step;
+      pin.set();
+      thread::sleep(time::Duration::new(0,duty_cycle));
+      pin.clear();
+      thread::sleep(time::Duration::new(0,interval - duty_cycle));
+    }
   }
 }
