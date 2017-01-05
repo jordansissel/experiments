cafd222e710832aa108049067a16a6d81a9054ef
Jordan Sissel <jls@semicomplete.com>
2017-01-04 17:12:19 -0800

    Yep.

--- docker/nextcloud/entry.sh
+++ docker/nextcloud/entry.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+set -x
+
 if [ ! -f "/srv/data/.setup_done" ] ; then
-  cp -R /var/www/nextcloud/data/ /srv/data/
+  cp -vR /var/www/nextcloud/data /srv/
+  touch /srv/data/.setup_done
+fi
+
+echo args $#
+echo "$@"
+
+if [ "$#" -eq 0 ] ; then
+  set -- apachectl -d /etc/apache2 -f /etc/apache2/apache2.conf -e info -DFOREGROUND
 fi
 
 exec "$@"
