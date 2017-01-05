ef61561b40d0d0e16997a74e75eb7514212d3f75
Jordan Sissel <jls@semicomplete.com>
2017-01-04 23:35:01 -0800

    Add letsencrypt.

--- docker/nextcloud/entry.sh
+++ docker/nextcloud/entry.sh
@@ -6,6 +6,13 @@ if [ ! -f "/srv/data/.setup_done" ] ; then
   cp -vR /var/www/nextcloud/data /srv/
   touch /srv/data/.setup_done
 fi
+ 
+if [ ! -d "/etc/letsencrypt/live" ] ; then
+  letsencrypt --apache certonly --agree-tos -d "$WEBDOMAIN" --email "$EMAIL"
+else
+  letsencrypt renew
+fi
+apachectl stop
 
 echo args $#
 echo "$@"
