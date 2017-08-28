3436b5b92d7396e31ff906ea8fe1f72a57350fee
Jordan Sissel <jls@semicomplete.com>
2017-08-28 12:51:16 -0700

    reject some default features of nashorn

--- java/nashorn/src/main/java/JSTest.java
+++ java/nashorn/src/main/java/JSTest.java
@@ -7,12 +7,18 @@ import javax.script.ScriptException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Reader;
+import java.util.Collections;
+import java.util.Map;
 
 public class JSTest {
     public static final String[] FLAGS = { "--no-java", "-strict" };
 
     public interface Console {
         void log(String arg);
+    }
+
+    public interface Bork {
+        public void bork() throws UnsupportedOperationException;
     }
 
     public static void main(String[] args) throws FileNotFoundException, ScriptException {
@@ -22,11 +28,31 @@ public class JSTest {
         ScriptEngine engine = ef.getScriptEngine(FLAGS);
         Bindings bindings = engine.createBindings();
 
-        String greeting = "Hello, world!";
+        bindings.put("$ARG", Collections.EMPTY_LIST);
+        bindings.put("$ENV", Collections.EMPTY_MAP);
+        bindings.put("$OPTIONS", "");
+        bindings.put("$OUT", null);
+        bindings.put("$ERR", null);
+        bindings.put("$EXIT", 0);
+        bindings.put("quit", (Bork)JSTest::unsupported);
+        bindings.put("exit", (Bork)JSTest::unsupported);
+        bindings.put("print", (Bork)JSTest::unsupported);
+        bindings.put("echo", (Bork)JSTest::unsupported);
+        bindings.put("readLine", (Bork)JSTest::unsupported);
+        bindings.put("load", (Bork)JSTest::unsupported);
+        bindings.put("loadWithNewGlobal", (Bork)JSTest::unsupported);
+        bindings.put("$EXEC", (Bork)JSTest::unsupported);
+
+        String greeting = "Hello";
         bindings.put("greeting", greeting);
         bindings.put("console", console);
 
         Reader script = new FileReader(args[0]);
         engine.eval(script, bindings);
     }
+
+    public static void unsupported() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("this function is unsupported");
+    }
 }
+
--- java/nashorn/src/main/resources/example.js
+++ java/nashorn/src/main/resources/example.js
@@ -1,1 +1,9 @@
+console.log(Math.PI);
 console.log(greeting);
+
+var x;
+for (x in this) {
+  console.log(x);
+}
+
+exit();
