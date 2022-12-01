dfe5b5045220446ee3bd0ce999b30730cb44b5b4
Jordan Sissel <jls@semicomplete.com>
2022-11-30 23:58:01 -0800

    Add 4th solution to part 1 which uses sed to generate ruby code. :P

--- aoc/aoc2022/1/shell.sh
+++ aoc/aoc2022/1/shell.sh
@@ -12,6 +12,9 @@ cat input | awk '/^$/ { a += 1 } !/^$/ { print > "elf."a }'; ls elf.* | xargs -n
 ## Solution 3, only use awk. Uses arrays inside awk.
 cat input | awk '/^$/ { a += 1 } !/^$/ { elf[a] += $1 } END { m = elf[0]; for (e in elf) { if (m < elf[e]) { m = elf[e] } }; print m }'
 
+## Solution 4, generate ruby with sed.
+cat input | sed -rne '/^$/!{ H }; /^$/ { x; s/(.)\n/\1+/g; s/^\n/puts /; p }' | ruby | sort -n | tail -1
+
 # Part 2
 # Solution 0, using awk|sort|tail
 cat input | awk '/^$/ { print sum; sum = 0 } !/^$/ { sum += $1 } END { print sum }' | sort -n |tail -3 | awk '{sum += $1} END { print sum }'
