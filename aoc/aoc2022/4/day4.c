c78bfccd0f38f02edd67c70d2a66025c74ef64af
Jordan Sissel <jls@semicomplete.com>
2022-12-03 22:46:21 -0800

    Add part 2.
    Also, for fun, add support for opening a file given as the first argument. If not given, uses stdin.

--- aoc/aoc2022/4/day4.c
+++ aoc/aoc2022/4/day4.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+// C doesn't typically have nice container or data structure classes
+// like you might find in Java, Python, etc, so we have to roll our own.
 typedef struct range {
     unsigned int start;
     unsigned int end;
@@ -18,23 +20,49 @@ int range_fully_contains(Range one, Range two) {
     return 0;
 }
 
+// Return 1 (true) if range one and range two overlap.
+// Returns 0 (false) otherwise.
+int range_any_overlap(Range one, Range two) {
+    // Check if either range bound is within the bounds of the other.
+    return (
+        (one.start >= two.start && one.start <= two.end)
+        || (one.end >= two.start && one.end <= two.end)
+        || (two.start >= one.start && two.start <= one.end)
+        || (two.end >= one.start && two.end <= one.end)
+    );
+}
+
 int main(int argc, char *argv[]) {
-    FILE *input = stdin; // XXX: Maybe we want to accept filename input via args?
+    FILE *input = stdin;
+    if (argc > 1) {
+        input = fopen(argv[1], "r");
+        if (input == NULL) {
+            char *errstr;
+            perror(errstr);
+            fprintf(stderr, "Error opening file, '%s': %s\n", argv[1], errstr);
+            return 1;
+        }
+    }
 
     char *data;
     size_t len;
 
-
     Range elf1, elf2;
 
-    //while ((data = fgetln(input, &len)) != NULL)
-
     unsigned int count_fully_contained = 0;
+    unsigned int count_any_contained = 0;
+
     while (fscanf(input, "%u-%u,%u-%u", &elf1.start, &elf1.end, &elf2.start, &elf2.end) > 0) {
         //printf("Range: %u - %u; %u - %u\n", elf1.start, elf1.end, elf2.start, elf2.end);
 
+        // Part 1, how many elf pairs have one range fully overlapping the other?
         if (range_fully_contains(elf1, elf2)) {
             count_fully_contained++;
+        }
+
+        // Part two, how many elf pairs have any overlapping jobs at all?
+        if (range_any_overlap(elf1, elf2)) {
+            count_any_contained++;
         }
     }
 
@@ -45,6 +73,7 @@ int main(int argc, char *argv[]) {
     }
 
     printf("Count of fully contained; %u\n", count_fully_contained);
+    printf("Count of any overlap: %u\n", count_any_contained);
 
     return 0;
 }
