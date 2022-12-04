9239824c67fb4750ea06f3135977349374e42138
Jordan Sissel <jls@semicomplete.com>
2022-12-03 23:05:02 -0800

    Refactor to have our enumerator yield pairs of elves instead of input strings

--- aoc/aoc2022/4/day4.rb
+++ aoc/aoc2022/4/day4.rb
@@ -1,26 +1,28 @@
 #/usr/bin/env ruby
 
-class Reader
+class ElfPairs
     include Enumerable
 
     def each
         while !ARGF.eof?
-            yield ARGF.readline.chomp
+            line = ARGF.readline.chomp
+            # Parse with the format with a regex.
+            # Uses capture groups in order to pick each number out of the line.
+            m = line.match(/(\d+)-(\d+),(\d+)-(\d+)/)
+            raise "Bug?" unless m
+
+            # Turn each text range "1-3" into a Range object 1..3
+
+            # Yield both ranges
+            yield [Range.new(*m[1..2].map(&:to_i)), Range.new(*m[3..4].map(&:to_i))]
         end
     end
 end
 
 full_overlap = 0
 any_overlap = 0
-Reader.new.each do |line|
-    # Parse with the format with a regex.
-    # Uses capture groups in order to pick each number out of the line.
-    m = line.match(/(\d+)-(\d+),(\d+)-(\d+)/)
-    raise "Bug?" unless m
 
-    # Turn each text range "1-3" into a Range object 1..3
-    elf1, elf2 = Range.new(*m[1..2].map(&:to_i)), Range.new(*m[3..4].map(&:to_i))
-
+ElfPairs.new.each do |elf1, elf2|
     full_overlap += 1 if elf1.cover?(elf2) || elf2.cover?(elf1)
 
     if elf1.cover?(elf2.min) || elf1.cover?(elf2.max) \
