fe39a8a0d50021a3e2a23711c1c2c17c8e1de545
Jordan Sissel <jls@semicomplete.com>
2025-05-11 23:55:49 -0700

    Completed demo

---
+++ emscripten/nethack-wipeout/wrapper.js
@@ -0,0 +1,11 @@
+"use strict"
+
+function wipeout_text (text, count, seed) {
+  const ptr = stringToNewUTF8(text);
+
+  _wipeout_text(ptr, count, seed);
+
+  result = UTF8ToString(ptr);
+  _free(ptr);
+  return result;
+}
