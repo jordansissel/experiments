8d8ee4b28d465773cd15e1efebc6fb2e0b965b51
Jordan Sissel <jls@semicomplete.com>
2025-10-11 17:50:17 -0700

    Remove old and deprecated

--- typescript/mocha/Foo.ts
+++ typescript/mocha/Foo.ts
@@ -6,7 +6,3 @@ export function Foo() {
   console.log(`Foo: Bar.Baz=${Bar.Baz}`);
   return Bar.Baz;
 }
-
-if (typeof process !== 'undefined' && import.meta.filename === process.argv[1]) {
-  Foo();
-}
